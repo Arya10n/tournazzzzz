00d941dba74f76c5c012f67e39938c73adcf6ea8
lokeshbanthia9 <banthialokesh@gmail.com>
2023-10-23 19:27:12 +0530

    hehe

--- public/assets/js/prod.env.js
+++
@@ -1,1 +0,0 @@
-const APIKEY = '69da076ba64349bfad936debef22f9ad'
--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authorize = require('../middleware/authorize');
-
-router.get('/', (req, res, next) => {
-    res.status(200).send('Profile Page');
-})
-
-module.exports = router;
